73b2799a52ed92a97e286db3b0ffb53ca693daa6
sz281234 <50170180+sz281234@users.noreply.github.com>
2023-04-25 23:40:57 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,3 +1,4 @@
-print "${global.text}"
-print "${global.password}"
-print "${global.dte}"
+from com.xebialabs.deployit.util import PasswordEncrypter
+username = "admin"
+adminPwd = PasswordEncrypter.getInstance().decrypt(releaseVariables['password'])
+request = HttpRequest({'url': 'http://localhost:5516/api/v1/config/system-message'}, username, adminPwd)
